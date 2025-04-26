497a99007ea0933c7d5dbdaea49b9e2b882d402b
Abyss <136069464+AbysSses@users.noreply.github.com>
2025-04-26 17:29:14 +0800

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -9,20 +9,3 @@ function userEleventySetup(eleventyConfig) {
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
 
-// 在src/helpers/userSetup.js中添加以下代码
-module.exports = {
-  beforeBuild: (eleventyConfig) => {
-    // 将HomePage文件复制到输出目录
-    eleventyConfig.addPassthroughCopy({ "homepage": "/" });
-  },
-  
-  directoryOutputPlugin: (directoryOutputConfig) => {
-    // 可以在这里添加自定义输出配置
-    return directoryOutputConfig;
-  },
-  
-  userComputation: (gardenData) => {
-    // 返回数据供模板使用
-    return {};
-  },
-};
